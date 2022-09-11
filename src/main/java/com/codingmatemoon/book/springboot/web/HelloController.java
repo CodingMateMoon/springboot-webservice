e21fd2983cea756924b33fe14618defb460da87b
codingmatemoon <abc9529@naver.com>
2022-09-12 06:44:20 +0900

    jsonPath : JSON 응답값을 필드별로 검증할 수 있는 메소드로 $를 기준으로 필드명을 명시합니다. $.name, $.amount로 검증합니다

--- src/main/java/com/codingmatemoon/book/springboot/web/HelloController.java
+++ src/main/java/com/codingmatemoon/book/springboot/web/HelloController.java
@@ -1,12 +1,24 @@
 package com.codingmatemoon.book.springboot.web;
 
+import com.codingmatemoon.book.springboot.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+
     @GetMapping("/hello")
     public String hello() {
         return "hello";
     }
+
+    // @RequestParam : 외부에서 API 호출시 전달하는 파라미터를 가져오는 어노테이션
+    @GetMapping("/hello/dto")
+    public HelloResponseDto hellDto(@RequestParam("name") String name, @RequestParam("amount") int amount){
+
+        return new HelloResponseDto(name, amount);
+    }
 }
+
+
--- src/test/java/com/codingmatemoon/book/springboot/web/HelloControllerTest.java
+++ src/test/java/com/codingmatemoon/book/springboot/web/HelloControllerTest.java
@@ -8,9 +8,12 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 
 
 @RunWith(SpringRunner.class)
@@ -25,5 +28,18 @@ public class HelloControllerTest {
         String hello = "hello";
 
         mvc.perform(get("/hello")).andExpect(status().isOk()).andExpect(content().string(hello));
+    }
+
+    // jsonPath : JSON 응답값을 필드별로 검증할 수 있는 메소드로 $를 기준으로 필드명을 명시합니다. $.name, $.amount로 검증합니다
+    @Test
+    public void helloDto가_리턴된다() throws Exception {
+        String name = "hello";
+        int amount = 1000;
+
+        mvc.perform(get("/hello/dto").param("name", name).param("amount", String.valueOf(amount))).andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is(name)))
+                .andExpect(jsonPath("$.amount", is(amount)));
+
+
     }
 }
