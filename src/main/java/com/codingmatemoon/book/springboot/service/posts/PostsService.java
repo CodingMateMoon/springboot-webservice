4c46077a9e6fd3e9583e7f6f5394f87a35b752a0
codingmatemoon <abc9529@naver.com>
2022-09-19 07:09:47 +0900

    JPA 영속성 컨텍스트(엔티티 영구 저장 환경)을 활용하여 Entity 객체의 값 변경시 트랜잭션을 종료할 때 테이블에 변경분 반영(dirty checking)

--- src/main/java/com/codingmatemoon/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/codingmatemoon/book/springboot/service/posts/PostsService.java
@@ -1,7 +1,10 @@
 package com.codingmatemoon.book.springboot.service.posts;
 
+import com.codingmatemoon.book.springboot.domain.posts.Posts;
 import com.codingmatemoon.book.springboot.domain.posts.PostsRepository;
+import com.codingmatemoon.book.springboot.web.dto.PostsResponseDto;
 import com.codingmatemoon.book.springboot.web.dto.PostsSaveRequestDto;
+import com.codingmatemoon.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,5 +17,19 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    // JPA 영속성 컨텍스트(엔티티 영구 저장 환경)을 활용하여 Entity 객체의 값 변경시 트랜잭션을 종료할 때 테이블에 변경분 반영(dirty checking)
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        return new PostsResponseDto(entity);
     }
 }
