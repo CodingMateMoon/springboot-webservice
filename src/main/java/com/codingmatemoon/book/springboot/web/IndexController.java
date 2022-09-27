b7bd416101bf43f9a2386e3e5b4bbadd0721baa4
codingmatemoon <abc9529@naver.com>
2022-09-28 07:16:07 +0900

    mustache 활용 게시글 목록 조회 Service 구현, PostsListResponseDto 추가

--- src/main/java/com/codingmatemoon/book/springboot/web/IndexController.java
+++ src/main/java/com/codingmatemoon/book/springboot/web/IndexController.java
@@ -1,14 +1,23 @@
 package com.codingmatemoon.book.springboot.web;
 
+import com.codingmatemoon.book.springboot.domain.posts.PostsRepository;
+import com.codingmatemoon.book.springboot.service.posts.PostsService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
 
+
+    private final PostsService postsService;
+
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("posts", postsService.findAllDesc());
         return "index";
     }
 
