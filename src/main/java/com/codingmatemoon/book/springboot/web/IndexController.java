8fa72fc6674e1c4d1e99170d671d2784678acf1d
codingmatemoon <abc9529@naver.com>
2022-10-06 07:25:11 +0900

    index.mustache 로그인 버튼 및 로그인 성공 시 사용자 이름을 보여주는 코드 추가. Controller에서 userName 필요한 정보 model에 저장

--- src/main/java/com/codingmatemoon/book/springboot/web/IndexController.java
+++ src/main/java/com/codingmatemoon/book/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.codingmatemoon.book.springboot.web;
 
+import com.codingmatemoon.book.springboot.config.auth.dto.SessionUser;
 import com.codingmatemoon.book.springboot.domain.posts.PostsRepository;
 import com.codingmatemoon.book.springboot.service.posts.PostsService;
 import com.codingmatemoon.book.springboot.web.dto.PostsResponseDto;
@@ -10,16 +11,24 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
-
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
+
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
+
         return "index";
     }
 
